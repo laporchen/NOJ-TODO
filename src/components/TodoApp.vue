94e8feb84b0442ecad73ae4841431d6738bca262
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2021-12-11 01:56:39 +0800

    fixed some minor bugs

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -4,30 +4,34 @@
     <!-- task,tag Input -->
     <div class="text-center mt-5">
       <!--TaskInput-->
-      <div class="d-flex">
+      <div class="d-flex justify-content-center">
         <input
           v-model="task"
           type="text"
           placeholder="Task here"
+          style="width: 60%"
           class="form-control text-center"
+          @keydown.enter="submitTask"
         />
       </div>
       <br />
       <!--TagInput-->
-      <div class="d-flex">
+      <div class="d-flex justify-content-center">
         <input
           v-model="tags"
           type="text"
+          style="width: 60%"
           placeholder="Tag here,with space in between"
           class="form-control text-center center-block"
         />
       </div>
       <br />
       <!--DateInput-->
-      <div class="d-flex">
+      <div class="d-flex justify-content-center">
         <input
           v-model="date"
           type="date"
+          style="width: 60%"
           placeholder="Date here"
           class="form-control text-center center-block"
         />
@@ -36,79 +40,97 @@
       <button @click="submitTask" class="btn btn-warning">SUMBIT</button>
     </div>
     <!-- filter-->
-    <div class="mt-5"><button @click="showAll" class="btn btn-warning">All</button><button @click="showTodo" class="btn btn-warning">Todo</button><button @click="showInProgress" class="btn btn-warning">In-progress</button></div>
+    <div class="mt-5 d-flex justify-content-center">
+      <div class="d-flex" style="width: 80%">
+        <button @click="showAll" style="width: 15%" class="btn btn-warning">All</button>
+        <button @click="showTodo" style="width: 15%" class="btn btn-warning">Todo</button>
+        <button @click="showInProgress" style="width: 15%" class="btn btn-warning">
+          In-progress
+        </button>
+        <button @click="save" style="width: 15%" class="ms-auto btn btn-warning">
+          Save
+        </button>
+      </div>
+    </div>
     <!-- show tasks -->
-    <table class="table table-bordered mt-1">
-      <thead>
-        <tr>
-          <th scope="col" style="width: 10%">Due Date</th>
-          <th scope="col" style="width: 45%">Task</th>
-          <th scope="col" style="width: 20%">Status</th>
-          <th scope="col" style="width: 15%">Tags</th>
-          <th scope="col" class="text-center" style="width: 5%">#</th>
-          <th scope="col" class="text-center" style="width: 5%">#</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(task, index) in tasks" :key="index">
-          <template v-if="viewState === 'ALL' || (viewState ==='TODO' && task.status === 'To-do') || (viewState === 'INPROGRESS' && task.status === 'In-progress')">
-          <td>
-            <!--date-->
-            <span>{{ task.date }}</span>
-          </td>
-          <td>
-            <!--name-->
-            <span :class="{ taskDone: task.status === 'Done' }">{{
-              task.name
-            }}</span>
-          </td>
-          <td>
-            <!--status-->
-            <span
-              @click="changeStatus(index)"
-              class="pointer"
-              :class="{
-                TextTodo: task.status === 'To-do',
-                TextProgress: task.status === 'In-progress',
-                TextDone: task.status === 'Done',
-              }"
-              >{{ task.status }}</span
+    <div class="d-flex justify-content-center">
+      <table class="table table-bordered mt-1 table" style="width: 80%">
+        <thead>
+          <tr>
+            <th scope="col" style="width: 10%">Due Date</th>
+            <th scope="col" style="width: 45%">Task</th>
+            <th scope="col" style="width: 20%">Status</th>
+            <th scope="col" style="width: 15%">Tags</th>
+            <th scope="col" class="text-center" style="width: 5%">#</th>
+            <th scope="col" class="text-center" style="width: 5%">#</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(task, index) in tasks" :key="index">
+            <template
+              v-if="
+                viewState === 'ALL' ||
+                (viewState === 'TODO' && task.status === 'To-do') ||
+                (viewState === 'INPROGRESS' && task.status === 'In-progress')
+              "
             >
-          </td>
-          <td>
-            <!--tag-->
-            <div class="text-center pointer">
-              <span v-for="(showtag, index) in task.tags" :key="index"
-                >{{ showtag }}<br
-              /></span>
-            </div>
-          </td>
-          <td>
-            <!--edit-->
-            <div class="text-center pointer" @click="editTask(index)">
-              <span class="fa fa-pen"></span>
-            </div>
-          </td>
-          <td>
-            <!--delete-->
-            <div class="text-center pointer" @click="deleteTask(index)">
-              <span class="fa fa-trash"></span>
-            </div>
-          </td>
-          </template>
-        </tr>
-        
-      </tbody>
-    </table>
+              <td>
+                <!--date-->
+                <span>{{ task.date }}</span>
+              </td>
+              <td>
+                <!--name-->
+                <span :class="{ taskDone: task.status === 'Done' }">{{ task.name }}</span>
+              </td>
+              <td>
+                <!--status-->
+                <span
+                  @click="changeStatus(index)"
+                  class="pointer"
+                  :class="{
+                    TextTodo: task.status === 'To-do',
+                    TextProgress: task.status === 'In-progress',
+                    TextDone: task.status === 'Done',
+                  }"
+                  >{{ task.status }}</span
+                >
+              </td>
+              <td>
+                <!--tag-->
+                <div class="text-center pointer">
+                  <span v-for="(showtag, index) in task.tags" :key="index"
+                    >{{ showtag }}<br
+                  /></span>
+                </div>
+              </td>
+              <td>
+                <!--edit-->
+                <div class="text-center pointer" @click="editTask(index)">
+                  <span class="fa fa-pen"></span>
+                </div>
+              </td>
+              <td>
+                <!--delete-->
+                <div class="text-center pointer" @click="deleteTask(index)">
+                  <span class="fa fa-trash"></span>
+                </div>
+              </td>
+            </template>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Todo",
   props: {
     msg: String,
   },
+
   data() {
     return {
       editingTask: null,
@@ -120,6 +142,16 @@ export default {
       tasks: [],
     };
   },
+  mounted() {
+    const that = this;
+    axios.get("http://127.0.0.1:8080/get_data", {}).then(function (response) {
+      if (response.data.status == "success") {
+        response.data.response.forEach((element) => {
+          that.tasks.push(element);
+        });
+      }
+    });
+  },
   methods: {
     addTag() {},
     submitTask() {
@@ -129,9 +161,14 @@ export default {
       if (this.editingTask !== null) {
         this.tasks[this.editingTask].name = this.task;
         this.tasks[this.editingTask].tags = this.tags.split(" ");
-        this.tasks[this.editingTask].date = this.date.replaceAll("-","/");
+        this.tasks[this.editingTask].date = this.date.replaceAll("-", "/");
         this.editingTask = null;
       } else {
+        if (this.tasks.find((x) => x.name === this.task) != undefined) {
+          alert("You had this thing in your list already.");
+          return;
+        }
+
         let newTag = this.tags;
         this.tasks.push({
           name: this.task,
@@ -143,9 +180,11 @@ export default {
       this.task = "";
       this.tags = "";
       this.date = "";
+      this.save();
     },
     deleteTask(index) {
       this.tasks.splice(index, 1);
+      this.save();
     },
     editTask(index) {
       this.task = this.tasks[index].name;
@@ -157,17 +196,26 @@ export default {
       let newIndex = this.allStatus.indexOf(this.tasks[index].status);
       this.tasks[index].status = this.allStatus[(newIndex + 1) % 3];
     },
-    showAll()
-    {
+    showAll() {
       this.viewState = "ALL";
     },
-    showTodo()
-    {
+    showTodo() {
       this.viewState = "TODO";
     },
-    showInProgress()
-    {
-      this.viewState = "INPROGRESS"
+    showInProgress() {
+      this.viewState = "INPROGRESS";
+    },
+    save() {
+      axios
+        .post("http://127.0.0.1:8080/savetoserver", {
+          info: JSON.stringify(this.tasks),
+        })
+        .then(function (response) {
+          var status = response.data.status;
+          if (status != "success") {
+            alert("Error!");
+          }
+        });
     },
   },
 };
@@ -189,13 +237,24 @@ export default {
 .TextDone {
   color: rgb(0, 202, 0);
 }
-.container > * {
+.container {
   color: white;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
 }
 
 .center-block {
   display: block;
   margin-right: auto;
   margin-left: auto;
+}
+th {
+  color: white;
+}
+table * {
+  color: white;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
